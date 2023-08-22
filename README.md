ee27426a856882eb3cc8c41c93feea67d39e2bea
= <28835813+ploop3@users.noreply.github.com>
2023-08-22 10:09:10 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-#Forkify Recipes App
+<h1 align="center">Forkify Recipes App</h1>
+<p align="center">
+    <img src="./src/img/favicon.png" alt="Logo" width="15%" height="auto">
+ </p>
+
+<h2 align="center">
+Check out the <a href="https://jocular-blancmange-3be22c.netlify.app/" target="_blank">live Forkify App here</a>
+</h2>
+<p align="left">
+Forkify is a recipe search and saving app, built with HTML5, CSS3, and JavaScript. It features an intuitive user interface and dynamic serving size adjustment. The app uses advanced JavaScript concepts like asynchronous programming, ES6 modules, and object-oriented programming. Forkify was created by Jonas Smechmann in his Udemy course and this app was built as part of a personal project to learn JavaScript.
+</p>
+
+## Features
+
+- Search for recipes: Find recipes using keywords or ingredients.
+- View recipe details: See ingredients, cooking instructions, and serving size.
+- Bookmark recipes: Save your favorite recipes for later.
+- Add recipes: Create and share your own recipes.
+- Pagination: Easily navigate search results.
+- Responsive Design
+- Consume REST API to GET and PUT recipes
+- Uses Model-View-Controller (MVC) arquitecture
+
+## Built with
+
+- JavaScript
+- CSS3
+- HTML5
+- Parcel
+- NPM
+
+<p align="center">
+  <img alt="Forkify App" src="./src/img/forkify-screenshot.png" width="100%" />
+</p>
+
+## Flowchart
+
+<p align="center">
+  <img alt="Flowchart" src="./forkify-flowchart-part-3.png" width="100%" />
+</p>
+
+## Recipe Loading Architecture
+
+<p align="center">
+  <img alt="Recipe Loading Architecture" src="./forkify-architecture-recipe-loading.png" width="100%" />
+</p>
+
+## Installation
+
+- Clone this repo:
+
+```sh
+git clone git@github.com:ploop3/Forkify.git
+```
+
+- Install dependencies:
+
+```sh
+npm install
+```
+
+- Run the app:
+
+```sh
+npm start
+```
+
+## Credits
+
+This app was created as part of [Jonas Smechmann](https://twitter.com/jonasschmedtman)'s Udemy course [The Complete JavaScript Course 2023: From Zero to Expert!](https://www.udemy.com/course/the-complete-javascript-course/).
+
+Special thanks to Jonas for his excellent teaching and guidance throughout the course.
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -51,7 +51,7 @@ class RecipeView extends View {
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="${icons}.svg#icon-clock"></use>
+              <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
               this._data.cookingTime
@@ -60,7 +60,7 @@ class RecipeView extends View {
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="${icons}.svg#icon-users"></use>
+              <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
               this._data.servings
@@ -72,14 +72,14 @@ class RecipeView extends View {
                 this._data.servings - 1
               }">
                 <svg>
-                  <use href="${icons}.svg#icon-minus-circle"></use>
+                  <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--update-servings" data-update-to="${
                 this._data.servings + 1
               } ">
                 <svg>
-                  <use href="${icons}.svg#icon-plus-circle"></use>
+                  <use href="${icons}#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -92,7 +92,7 @@ class RecipeView extends View {
           </div>
           <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}.svg#icon-bookmark${
+              <use href="${icons}#icon-bookmark${
       this._data.bookmarked ? '-fill' : ''
     }"></use>
             </svg>
@@ -124,7 +124,7 @@ class RecipeView extends View {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="${icons}.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </a>
         </div>
@@ -135,7 +135,7 @@ class RecipeView extends View {
     return `
       <li class="recipe__ingredient">
         <svg class="recipe__icon">
-          <use href="${icons}.svg#icon-check"></use>
+          <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
           ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
